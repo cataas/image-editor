caeb0779a9580d235e62148b24f4fb33683ad175
Kevin Balicot <kevinbalicot@gmail.com>
2017-07-05 10:46:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,191 @@
+# Image editor
+
+A image editor library
+
+## Example
+
+```javascript
+fs.readFile('my-file.png', 'utf-8', (err, buffer) => {
+    imageEditor.edit(buffer, 'image/png', 'square', 'Hello')
+        .then(buffer => fs.writeFile('output.png', buffer));
+});
+```
+
+# API Reference
+
+## ImageEditor
+
+* ImageEditor
+    * ImageEditor#edit(buffer, mimetype, [type], [text], [color], [fontSize], [filter], [customWidth], [customHeight]) => <code>Promise</code>
+    * ImageEditor#applyType(buffer, mimetype, [type], [customWidth], [customHeight]) => <code>Promise</code>
+    * ImageEditor#applyFilter(buffer, mimetype, filter) => <code>Promise</code>
+    * ImageEditor#size(buffer) => <code>Promise</code>
+    * ImageEditor#write(buffer, text, [width], [height], [position], [color], [fontSize]) => <code>Promise</code>
+    * ImageEditor#resize(buffer, width, [height], [option]) => <code>Promise</code>
+    * ImageEditor#crop(buffer, width, height, [gravity], [x], [y]) => <code>Promise</code>
+    * ImageEditor#blur(buffer, radius, sigma) => <code>Promise</code>
+    * ImageEditor#mono(buffer) => <code>Promise</code>
+    * ImageEditor#sepia(buffer) => <code>Promise</code>
+    * ImageEditor#mosaic(buffer) => <code>Promise</code>
+    * ImageEditor#negative(buffer) => <code>Promise</code>
+    * ImageEditor#paint(buffer, radius&#x60;) => <code>Promise</code>
+    * ImageEditor#pixelate(buffer, mimetype, size) => <code>Promise</code>
+
+### ImageEditor#edit(buffer, mimetype, [type], [text], [color], [fontSize], [filter], [customWidth], [customHeight])=> <code>Promise</code>
+Edit an image
+
+**Kind**: Exported function  
+
+| Param | Type | Default |
+| --- | --- | --- |
+| buffer | <code>Buffer</code> |  |
+| mimetype | <code>string</code> |  |
+| [type] | <code>string</code> | <code>&quot;default&quot;</code> |
+| [text] | <code>string</code> | <code>null</code> |
+| [color] | <code>string</code> | <code>&quot;white&quot;</code> |
+| [fontSize] | <code>number</code> | <code>30</code> |
+| [filter] | <code>string</code> | <code>null</code> |
+| [customWidth] | <code>number</code> | <code></code> |
+| [customHeight] | <code>number</code> | <code></code> |
+
+### ImageEditor#applyType(buffer, mimetype, [type], [customWidth], [customHeight]) => <code>Promise</code>
+Apply type
+
+**Kind**: Exported function  
+
+| Param | Type | Default |
+| --- | --- | --- |
+| buffer | <code>Buffer</code> |  |
+| mimetype | <code>string</code> |  |
+| [type] | <code>string</code> | <code>&quot;default&quot;</code> |
+| [customWidth] | <code>number</code> | <code></code> |
+| [customHeight] | <code>number</code> | <code></code> |
+
+### ImageEditor#applyFilter(buffer, mimetype, filter) => <code>Promise</code>
+Apply filters
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+| mimetype | <code>string</code> |
+| filter | <code>string</code> |
+
+### ImageEditor#size(buffer) => <code>Promise</code>
+Get size of picture
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+
+### ImageEditor#write(buffer, text, [width], [height], [position], [color], [fontSize]) => <code>Promise</code>
+Write something on picture
+
+**Kind**: Exported function  
+
+| Param | Type | Default |
+| --- | --- | --- |
+| buffer | <code>Buffer</code> |  |
+| text | <code>string</code> |  |
+| [width] | <code>number</code> | <code>0</code> |
+| [height] | <code>number</code> | <code>0</code> |
+| [position] | <code>string</code> | <code>&quot;Center&quot;</code> |
+| [color] | <code>string</code> | <code>&quot;white&quot;</code> |
+| [fontSize] | <code>number</code> | <code>30</code> |
+
+### ImageEditor#resize(buffer, width, [height], [option]) => <code>Promise</code>
+Resize picture
+
+**Kind**: Exported function  
+
+| Param | Type | Default |
+| --- | --- | --- |
+| buffer | <code>Buffer</code> |  |
+| width | <code>number</code> |  |
+| [height] | <code>number</code> | <code></code> |
+| [option] | <code>string</code> | <code>null</code> |
+
+### ImageEditor#crop(buffer, width, height, [gravity], [x], [y]) => <code>Promise</code>
+Crop picture
+
+**Kind**: Exported function  
+
+| Param | Type | Default |
+| --- | --- | --- |
+| buffer | <code>Buffer</code> |  |
+| width | <code>number</code> |  |
+| height | <code>number</code> |  |
+| [gravity] | <code>string</code> | <code>&quot;Center&quot;</code> |
+| [x] | <code>number</code> | <code>0</code> |
+| [y] | <code>number</code> | <code>0</code> |
+
+### ImageEditor#blur(buffer, radius, sigma) => <code>Promise</code>
+Blur image
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+| radius | <code>number</code> |
+| sigma | <code>string</code> |
+
+### ImageEditor#mono(buffer) => <code>Promise</code>
+Mono
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+
+### ImageEditor#sepia(buffer) => <code>Promise</code>
+Sepia
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+
+### ImageEditor#mosaic(buffer) => <code>Promise</code>
+Mosaic
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+
+### ImageEditor#negative(buffer) => <code>Promise</code>
+Negative
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+
+### ImageEditor#paint(buffer, radius&#x60;) => <code>Promise</code>
+Paint
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+| radius` | <code>number</code> |
+
+### ImageEditor#pixelate(buffer, mimetype, size) => <code>Promise</code>
+Pixelate
+
+**Kind**: Exported function  
+
+| Param | Type |
+| --- | --- |
+| buffer | <code>Buffer</code> |
+| mimetype | <code>string</code> |
+| size | <code>number</code> |
---
+++ index.js
@@ -0,0 +1,426 @@
+const gm = require('gm');
+const jimp = require('jimp');
+
+/**
+ * @module ImageEditor
+ * @example
+ * const fs = require('fs');
+ * const imageEditor = require('image-editor');
+ *
+ * fs.readFile('my-file.png', 'utf-8', (err, buffer) => {
+ *     imageEditor.edit(buffer, 'image/png', 'square', 'Hello')
+ *         .then(buffer => {});
+ * });
+ */
+class ImageEditor {
+
+    constructor() {
+        this.font = 'Arial';
+    }
+
+    /**
+     * Edit an image
+     * @param {Buffer} buffer
+     * @param {string} mimetype
+     * @param {string} [type=default]
+     * @param {string} [text=null]
+     * @param {string} [color=white]
+     * @param {number} [fontSize=30]
+     * @param {string} [filter=null]
+     * @param {number} [customWidth=null]
+     * @param {number} [customHeight=null]
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    edit(buffer, mimetype, type = 'default', text = '', color = '#ffffff', fontSize = 30, filter = null, customWidth = null, customHeight = null) {
+
+        return this.applyType(buffer, mimetype, type, customWidth, customHeight)
+            .then(buffer => this.applyFilter(buffer, mimetype, filter))
+            .then(buffer => this.size(buffer))
+            .then(data => {
+                let heightText = Math.floor((data.sizes.height / 2) / 2);
+                return this.write(data.buffer, text, 0, heightText, 'Center', color, fontSize);
+            })
+            .catch(err => console.log(err))
+        ;
+    }
+
+    /**
+     * Apply type
+     * @param {Buffer} buffer
+     * @param {string} mimetype
+     * @param {string} [type=default]
+     * @param {number} [customWidth=null]
+     * @param {number} [customHeight=null]
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    applyType(buffer, mimetype, type = 'default', customWidth = null, customHeight = null) {
+
+        if (type === 'sq' || type === 'square') {
+            let gravity = mimetype === 'image/gif' ? 'NorthWest' : 'Center';
+            return this.resize(buffer, 200, '200^').then(buffer => this.crop(buffer, 200, '200^', gravity));
+        }
+
+        if (type === 'md' || type === 'medium') {
+            return this.resize(buffer, 400);
+        }
+
+        if (type === 'sm' || type === 'small') {
+            return this.resize(buffer, 300);
+        }
+
+        if (type === 'xs' || type === 'xsmall') {
+            return this.resize(buffer, 100);
+        }
+
+        if (type === 'or' || type === 'original') {
+            return new Promise((resolve, reject) => resolve(buffer));
+        }
+
+        if (customWidth !== null || customHeight !== null) {
+            return this.resize(buffer, customWidth, customHeight);
+        }
+
+        return this.resize(buffer, 600);
+    }
+
+    /**
+     * Apply filters
+     * @param {Buffer} buffer
+     * @param {string} mimetype
+     * @param {string} filter
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    applyFilter(buffer, mimetype, filter) {
+
+        if (filter === 'blur') {
+            return this.blur(buffer, 5, 2);
+        }
+
+        if (filter === 'mono') {
+            return this.mono(buffer);
+        }
+
+        if (filter === 'sepia') {
+            return this.sepia(buffer);
+        }
+
+        if (filter === 'mosaic') {
+            //return this.mosaic(buffer);
+        }
+
+        if (filter === 'pixel') {
+            return this.pixelate(buffer, mimetype, 8);
+        }
+
+        if (filter === 'negative') {
+            return this.negative(buffer);
+        }
+
+        if (filter === 'paint') {
+            return this.paint(buffer, 5);
+        }
+
+        return new Promise((resolve, reject) => resolve(buffer));
+    }
+
+    /**
+     * Get size of picture
+     * @param {Buffer} buffer
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    size(buffer) {
+        return new Promise((resolve, reject) => {
+
+            gm(buffer).size((err, sizes) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ sizes: sizes, buffer: buffer });
+                }
+            });
+        });
+    }
+
+    /**
+     * Write something on picture
+     * @param {Buffer} buffer
+     * @param {string} text
+     * @param {number} [width=0]
+     * @param {number} [height=0]
+     * @param {string} [position=Center]
+     * @param {string} [color=white]
+     * @param {number}  [fontSize=30]
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    write(buffer, text, width = 0, height = 0, position = 'Center', color = '#ffffff', fontSize = 30) {
+
+        return new Promise((resolve, reject) => {
+
+            if (text === '' || text === null) {
+                return resolve(buffer);
+            }
+
+            gm(buffer)
+                .fill('#000000')
+                .font(this.font, fontSize)
+                .drawText((width + 1), (height + 1), text, position)
+                .fill(color)
+                .font(this.font, fontSize)
+                .drawText(width, height, text, 'Center')
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Resize picture
+     * @param {Buffer} buffer
+     * @param {number} width
+     * @param {number} [height=null]
+     * @param {string} [option=null]
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    resize(buffer, width, height = null, option = null) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .resize(width, height, option)
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Crop picture
+     * @param {Buffer} buffer
+     * @param {number} width
+     * @param {number} height
+     * @param {string} [gravity=Center]
+     * @param {number} [x=0]
+     * @param {number} [y=0]
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    crop(buffer, width, height, gravity = 'Center', x = 0, y = 0) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .gravity(gravity)
+                .crop(width, height, x ,y)
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Blur image
+     * @param {Buffer} buffer
+     * @param {number} radius
+     * @param {string} sigma
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    blur(buffer, radius, sigma) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .blur(radius, sigma)
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Mono
+     * @param {Buffer} buffer
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    mono(buffer) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .monochrome()
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Sepia
+     * @param {Buffer} buffer
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    sepia(buffer) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .sepia()
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Mosaic
+     * @param {Buffer} buffer
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    mosaic(buffer) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .mosaic()
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Negative
+     * @param {Buffer} buffer
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    negative(buffer) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .negative()
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Paint
+     * @param {Buffer} buffer
+     * @param {number} radius`
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    paint(buffer, radius) {
+
+        return new Promise((resolve, reject) => {
+            gm(buffer)
+                .paint(radius)
+                .toBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                })
+            ;
+        });
+    }
+
+    /**
+     * Pixelate
+     * @param {Buffer} buffer
+     * @param {string} mimetype
+     * @param {number} size
+     *
+     * @returns {Promise}
+     *
+     * @alias module:ImageEditor
+     */
+    pixelate(buffer, mimetype, size) {
+
+        return new Promise((resolve, reject) => {
+            jimp.read(buffer).then(image => {
+                image.pixelate(size).getBuffer(mimetype, (err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                });
+            }).catch(err => reject(err));
+        });
+    }
+}
+
+module.exports = new ImageFactory();
